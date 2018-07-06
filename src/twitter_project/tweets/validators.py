a4dbc21baf64cd37a1936b606adf5fc9a82635f7
Tsenddavaa Ganbat <tsenddavaa@sdfutures.org>
2018-07-05 18:15:08 -0700

    hee

--- src/twitter_project/tweets/validators.py
+++ src/twitter_project/tweets/validators.py
@@ -2,6 +2,6 @@ from django.core.exceptions import ValidationError
 
 def validate_content(value):
 	content = value
-	if content == "damn":
+	if content == "":
 		raise ValidationError("Content cannot be profanity. Just Write a NICE Tweet")
 	return value
